594bd3223d623d5333001fb4302db8dbdfca8a58
Christopher Carter <chrisjohncarter123@gmail.com>
2020-03-08 07:36:24 +0000

    first commit

---
+++ lib/wisdom/mine.rb
@@ -0,0 +1,8 @@
+require "wisdom/mine/version"
+
+module Wisdom
+  module Mine
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/wisdom/mine/version.rb
@@ -0,0 +1,5 @@
+module Wisdom
+  module Mine
+    VERSION = "0.1.0"
+  end
+end
